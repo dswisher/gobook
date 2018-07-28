b28306d03cb5527bab3cb4950365883e5a8a5899
Doug Swisher <big.swish@gmail.com>
2018-07-27 20:48:30 -0500

    Fix infinite loop

--- ch3/mandelbrot/main.go
+++ ch3/mandelbrot/main.go
@@ -17,7 +17,7 @@ func main() {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
-		for px := 0; py < width; px++ {
+		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
 			// Image point (px, py) represents complex value z
@@ -30,9 +30,10 @@ func main() {
 func mandelbrot(z complex128) color.Color {
 	const iterations = 200
 	const contrast = 15
+
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
-		v := v*v + z
+		v = v*v + z
 		if cmplx.Abs(v) > 2 {
 			return color.Gray{255 - contrast*n}
 		}
